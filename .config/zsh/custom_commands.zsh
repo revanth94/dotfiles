7a5e1f6f1124ce7edba32e5013d97a329743c1df
Revanth Korrapati <revanth@Revanths-MacBook-Pro.local>
2025-07-29 21:20:38 +0530

    Cleaning up custom comands

--- .config/zsh/custom_commands.zsh
+++ .config/zsh/custom_commands.zsh
@@ -4,8 +4,8 @@
 alias python='python3'
 
 # Switch between bash and zsh
-alias switch_to_zsh='chsh -s /bin/zsh'
-alias switch_to_bash='chsh -s /bin/bash'
+# alias switch_to_zsh='chsh -s /bin/zsh'
+# alias switch_to_bash='chsh -s /bin/bash'
 
 
 # Custom commands and aliases
@@ -13,7 +13,6 @@ CUSTOM_COMMANDS_FILE=~/.config/zsh/custom_commands.zsh
 
 alias view_custom_commands='cat '$CUSTOM_COMMANDS_FILE
 alias edit_custom_commands='vi '$CUSTOM_COMMANDS_FILE
-
 alias reload_custom_commands='source '$CUSTOM_COMMANDS_FILE
 
 function timer(){
@@ -25,76 +24,6 @@ function timer(){
 }
 
 mkcd () { mkdir "$1" && cd "$1"; }
-
-alias python='python3'
-
-remove_folder_if_exists () { if [ -d $1 ] ; then rm -r $1 ; fi; }
-
-
-function safe() {
-	remove_folder_if_exists /Users/revanth/Downloads/Brave/Pics
-	remove_folder_if_exists /Users/revanth/Downloads/Brave/All
-	remove_folder_if_exists /Users/revanth/Downloads/Brave/DWP
-	remove_folder_if_exists /Users/revanth/Downloads/Brave/Temp
-}
-
-
-# File and Folder mods
-function head_stripper() {
-	dirname=$1
-	result="${dirname%"${dirname##*[!/]}"}" # extglob-free multi-trailing-/ trim
-	result="${result##*/}"                  # remove everything before the last /
-	result=${result:-/}
-	echo $result
-}
-
-function get_extension() {
-	filename=$1
-	shopt -s extglob           # enable +(...) glob syntax
-	result=${filename%%+(/)}    # trim however many trailing slashes exist
-	result=${result##*.}       # remove everything before the last / that still remains
-	result=${result:-/}        # correct for dirname=/ case
-	printf '%s\n' "$result"
-}
-
-function custom_echo () {
-	echo "Folder: $1"
-}
-
-function nested_executor () {
-	folder=$1
-	command=$2
-	for dir in $folder/*; do 
-	if [ -d $dir ]; then 
-	echo "Folder: $dir";
-	# custom_echo $dir
-	$command "$dir"
-	fi; 
-	done
-}
-
-function file_renamer () {
-	echo "Renaming files in folder: $1"
-	folder=$1
-	command=$2
-	tempdir=$1/renamer_temp
-	mkdir $tempdir
-	x=1
-	folder_name=$(head_stripper $folder)
-	echo "Folder Name: $folder_name"
-	for file in $folder/*; do 
-	if [ -f "$file" ]; then 
-	
-	extension=$(get_extension "$file")
-	mv "$file" "$tempdir/$folder_name$x.$extension"
-	x=$(expr $x + 1);
-
-	fi; 
-	done
-	mv $tempdir/* $1
-	rmdir $tempdir
-}
-
 
 function oapp () {
   open -a "$1" $2
